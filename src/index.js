092c81cda6fe124f751fea71a863af111ca9227b
Alena Ahafonova <cool-5@tut.by>
2021-02-22 13:47:30 +0300

    refactor: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    str.split('').forEach(function(item, i, arr)  {
+        
+        if (i == 0) {
+            stack.push(item);
+            return;
+        }
+
+        let lastOpen = bracketsConfig.find(type =>
+            stack[stack.length - 1] == type[0]);
+
+        lastOpen = lastOpen && lastOpen[1];
+
+        if (item != lastOpen) {
+            stack.push(item);
+        } else {
+            stack.pop();
+        }
+
+    });
+
+    if (stack.length == 0) return true;
+    return false;
 }
